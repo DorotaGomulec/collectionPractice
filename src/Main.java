16b8f12d2db143d4c5fe8918d698a314f3b240b2
dorotagomulec <doramed8@wp.pl>
2023-07-10 11:02:18 +0200

    options ideas

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println( "Hello world!" );
+
+
+
+
+        int userChoice;
+       switch (userChoice) {
+           case 1 -> "Add Player";
+           case 1 -> "Add Staff Employee";
+           case 2 -> "Show all Employee";
+           case 3 -> "Show players";
+           case 4 -> "Show staff";
+           case 5 -> "Sort Player by Heigh";
+           case 6 -> "Sort Staff by name";
+           case 7 -> "Find Employee by name";
+           case 7 -> "Find Player by field position";
+           case 8 -> "Count players";
+           case 8 -> "Back to menu";
+       }
     }
 }
