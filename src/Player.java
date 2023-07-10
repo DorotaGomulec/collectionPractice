c9d8139d105654590e41de66764d0a2de9bc3a82
dorotagomulec <doramed8@wp.pl>
2023-07-10 10:45:56 +0200

    classed created

--- src/Player.java
+++ src/Player.java
@@ -2,7 +2,27 @@
 
 public class Player extends Employee{
     PlayerPosition playerPosition;
-    int number;
     int height;
 
+    public Player(String name, int age, PlayerPosition playerPosition, int number, int height) {
+        super( name, age);
+        this.playerPosition = playerPosition;
+        this.height = height;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public PlayerPosition getPlayerPosition() {
+        return playerPosition;
+    }
+
+    public void setPlayerPosition(PlayerPosition playerPosition) {
+        this.playerPosition = playerPosition;
+    }
 }
--- src/Staff.java
+++ src/Staff.java
@@ -1,4 +1,8 @@
 public class Staff extends Employee{
     StaffRole staffRole;
 
+    public Staff(String name, int age, StaffRole staffRole) {
+        super( name, age);
+        this.staffRole = staffRole;
+    }
 }
